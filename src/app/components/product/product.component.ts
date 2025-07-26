dcfda03c242eb55ac787c7a1337ffe2590716197
Gemy33 <mohamedgamalabdelhaliym@gmail.com>
2025-07-26 19:40:55 +0200

    end from sorting and updata categoriy section design

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,21 +3,30 @@ import { ProductsService } from '../../services/products.service';
 import { Iproduct } from '../../interfaces/product';
 import { CurrencyPipe, NgFor } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe,NgFor,RouterLink],
+  imports: [CurrencyPipe,NgFor,RouterLink,FormsModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit {
+  products: any[] = [];
+filteredProducts: any[] = [];
+searchTerm: string = '';
+sortOption: string = '';
   constructor(private ProductsService : ProductsService){}
   allProducts:Iproduct[]=[]
   ngOnInit(): void {
+    
+    
     this.ProductsService.getAllProduct().subscribe({
       next:(res=> {
         console.log(res)
+         this.products = res;
+    this.filteredProducts = [...res];
         this.allProducts=res;
         // console.log(this.allProducts);
         
@@ -29,5 +38,33 @@ export class ProductComponent implements OnInit {
      
     })
   }
+
+  onSearchChange() {
+  this.applyFilters();
+}
+
+onSortChange() {
+  this.applyFilters();
+}
+
+applyFilters() {
+  let filtered = this.products.filter((product) =>
+    product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+  );
+
+  switch (this.sortOption) {
+    case 'priceLowHigh':
+      filtered.sort((a, b) => a.price - b.price);
+      break;
+    case 'priceHighLow':
+      filtered.sort((a, b) => b.price - a.price);
+      break;
+    case 'nameAZ':
+      filtered.sort((a, b) => a.title.localeCompare(b.title));
+      break;
+  }
+
+  this.allProducts = filtered;
+}
 
 }
